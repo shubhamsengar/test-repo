a1973b64c2a4971fb90dbbe586a8fcff10d832b3
Shubham <shubhamsengar88@gmail.com>
2024-09-23 08:40:03 +0530

    fix: modified release config

--- release.config.js
+++ release.config.js
@@ -1,23 +1,6 @@
 module.exports = {
   branches: ['main'],
   preset: 'conventionalcommits',
-  presetConfig: {
-    types: [
-      { type: 'feat', section: 'Features' },
-      { type: 'fix', section: 'Bug Fixes' },
-      { type: 'chore', section: 'Chores' },
-      { type: 'docs', hidden: true },
-      { type: 'style', hidden: true },
-      { type: 'refactor', section: 'Refactoring' },
-      { type: 'perf', hidden: true },
-      { type: 'test', hidden: true },
-    ],
-  },
-  releaseRules: [
-    { type: 'refactor', release: 'patch' },
-    { breaking: true, release: 'minor' },
-    { tag: 'Breaking', release: 'major' },
-  ],
   tagFormat: 'v${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
