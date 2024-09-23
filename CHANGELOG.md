e232c77bdd1c8058d454cb7cb0e8417b01cb7f24
Shubham <shubhamsengar88@gmail.com>
2024-09-23 10:42:43 +0530

    feat!: concluding semantic release learning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,55 @@
+## [2.0.1](https://github.com/shubhamsengar/test-repo/compare/v2.0.0...v2.0.1) (2024-09-23)
+
+### Bug Fixes
+
+* modified release config ([a1973b6](https://github.com/shubhamsengar/test-repo/commit/a1973b64c2a4971fb90dbbe586a8fcff10d832b3))
+
+## [2.0.0](https://github.com/shubhamsengar/test-repo/compare/v1.8.0...v2.0.0) (2024-09-22)
+
+### ⚠ BREAKING CHANGES
+
+* f12 added
+
+### Features
+
+* f12 added ([7a2bc91](https://github.com/shubhamsengar/test-repo/commit/7a2bc9149cce67c60ea7b5bc4b5358a752d54462))
+
+## [1.8.0](https://github.com/shubhamsengar/test-repo/compare/v1.7.0...v1.8.0) (2024-09-22)
+
+### Features
+
+* [Breaking] f11 added ([5fadc69](https://github.com/shubhamsengar/test-repo/commit/5fadc6908fe4b19cefcf0dc885971ea59e88b1db))
+
+## [1.7.0](https://github.com/shubhamsengar/test-repo/compare/v1.6.0...v1.7.0) (2024-09-22)
+
+### ⚠ BREAKING CHANGES
+
+* f10 added
+
+### Features
+
+* f10 added ([8b2a9fc](https://github.com/shubhamsengar/test-repo/commit/8b2a9fce37b44bd5100c24c1ba02438433651ea2))
+
+## [1.6.0](https://github.com/shubhamsengar/test-repo/compare/v1.5.0...v1.6.0) (2024-09-22)
+
+### Features
+
+* [Breaking] feature 9 added ([85e2a5e](https://github.com/shubhamsengar/test-repo/commit/85e2a5eb16d4e53d89eb97dd486ef25d47c87058))
+
+## [1.5.0](https://github.com/shubhamsengar/test-repo/compare/v1.4.0...v1.5.0) (2024-09-22)
+
+### Features
+
+* f7 added ([95ba36c](https://github.com/shubhamsengar/test-repo/commit/95ba36c9c25e50501496939e5a9a3190b87c177b))
+* feature 8 added ([6b06743](https://github.com/shubhamsengar/test-repo/commit/6b0674303595afb61f4350a2b23917c5cb005ce8))
+
+## [1.4.0](https://github.com/shubhamsengar/test-repo/compare/v1.3.0...v1.4.0) (2024-09-22)
+
+### Features
+
+* added config files for semantic release ([4df7a21](https://github.com/shubhamsengar/test-repo/commit/4df7a21d89d428f7ce61bad2aee77f8c8b2b7b46))
+* feature 6 added ([f827bd9](https://github.com/shubhamsengar/test-repo/commit/f827bd95dfad1883fe26c16bb523ed024675753f))
+
 ## [1.3.0](https://github.com/shubhamsengar/test-repo/compare/v1.2.0...v1.3.0) (2024-09-22)
 
 ### ⚠ BREAKING CHANGES
--- release.config.js
+++ release.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   branches: ['main'],
   preset: 'conventionalcommits',
+  presetConfig: {
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'chore', section: 'Chores' },
+      { type: 'docs', hidden: true },
+      { type: 'style', hidden: true },
+      { type: 'refactor', section: 'Refactoring' },
+      { type: 'perf', hidden: true },
+      { type: 'test', hidden: true },
+    ],
+  },
+  releaseRules: [
+    { type: 'refactor', release: 'patch' },
+    { breaking: true, release: 'minor' },
+    { tag: 'Breaking', release: 'major' },
+  ],
   tagFormat: 'v${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
@@ -13,4 +30,81 @@ module.exports = {
     ],
   ],
   };
+
+//   const { execSync } = require('child_process');
+// module.exports = isDryRun() ? getDryRunConfig() : getCIConfig();
+
+// function getDryRunConfig() {
+//   return {
+//     repositoryUrl: getLocalRepoUrl(),
+//     branches: getCurrentBranch(),
+//     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator'],
+//   };
+// }
+
+// function isDryRun() {
+//   return process.argv.includes('--dry-run');
+// }
+
+// function getLocalRepoUrl() {
+//   const topLevelDir = execSync('git rev-parse --show-toplevel').toString().trim();
+
+//   return `file://${topLevelDir}/.git`;
+// }
+
+// function getCurrentBranch() {
+//   return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+// }
+
+// /**
+//  * @type {import('semantic-release').GlobalConfig}
+//  */
+// function getCIConfig() {
+//   return {
+//     branches: ['main', 'chore/fix-semantic'],
+//     preset: 'conventionalcommits',
+//     presetConfig: {
+//       types: [
+//         { type: 'feat', section: 'Features' },
+//         { type: 'fix', section: 'Bug Fixes' },
+//         { type: 'chore', section: 'Chores' },
+//         { type: 'docs', hidden: true },
+//         { type: 'style', hidden: true },
+//         { type: 'refactor', section: 'Refactoring' },
+//         { type: 'perf', hidden: true },
+//         { type: 'test', hidden: true },
+//       ],
+//     },
+//     releaseRules: [
+//       { type: 'refactor', release: 'patch' },
+//       { breaking: true, release: 'minor' },
+//       { tag: 'Breaking', release: 'major' },
+//     ],
+//     tagFormat: 'v${version}',
+//     plugins: [
+//       '@semantic-release/commit-analyzer',
+//       '@semantic-release/release-notes-generator',
+//       [
+//         '@semantic-release/changelog',
+//         {
+//           changelogFile: `./CHANGELOG.md`,
+//         },
+//       ],
+//       [
+//         '@semantic-release/exec',
+//         {
+//           prepareCmd: `VERSION=\${nextRelease.version} npx -p replace-json-property rjp ./package.json version \${nextRelease.version} && npm i && echo nextVer=\${nextRelease.version} >> $GITHUB_OUTPUT`,
+//         },
+//       ],
+//       [
+//         '@semantic-release/git',
+//         {
+//           assets: [`package.json`, `package-lock.json`, `CHANGELOG.md`],
+//           message: 'chore(release): -v${nextRelease.version} \n\n${nextRelease.notes}',
+//         },
+//       ],
+//     ],
+//   };
+// }
+
   
